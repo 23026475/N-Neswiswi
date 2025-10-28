42e1fe99c832b237c5a489245e45c493c4d65dd8
23026475 <ndivhuswiswi@gmail.com>
2025-10-28 03:09:25 +0200

    refactor home/projects pages

--- ndivhuwo-portfolio/app/page.tsx
+++ ndivhuwo-portfolio/app/page.tsx
@@ -1,14 +1,18 @@
-import Navbar from "@/components/Navbar";
-import SideNav from "@/components/SideNav";
+"use client";
+
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="relative">
-      <Navbar />
-      <SideNav />
-      <section id="hero"><Hero /></section>
-      {/* Later we’ll add <Projects /> and <Contact /> */}
+      {/* Hero Section */}
+      <section id="hero">
+        <Hero />
+      </section>
+
+      {/* Placeholder for future sections */}
+      {/* <section id="projects"><Projects /></section> */}
+      {/* <section id="contact"><Contact /></section> */}
     </main>
   );
 }
