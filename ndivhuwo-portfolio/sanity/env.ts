c1d26d0f884f5e37797347b76628a9d78bef8a5f
23026475 <ndivhuswiswi@gmail.com>
2025-10-28 14:47:10 +0200

    Update Sanity environment configuration - Updated project ID, dataset, and API version

--- ndivhuwo-portfolio/sanity/env.ts
+++ ndivhuwo-portfolio/sanity/env.ts
@@ -1,20 +1,12 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-10-28'
+export const apiVersion = '2025-10-28'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = 'growthblog'
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = 'rwizuy4v'
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage)
   }
-
   return v
 }
