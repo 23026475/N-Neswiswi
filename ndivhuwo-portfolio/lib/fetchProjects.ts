8ee251a8fa3f28d2394d7980b92ad11de2adec3c
23026475 <ndivhuswiswi@gmail.com>
2025-10-31 21:14:29 +0200

    feat(projects): add fetchProjects utility for retrieving Sanity project data

---
+++ ndivhuwo-portfolio/lib/fetchProjects.ts
@@ -0,0 +1,33 @@
+import { fetchSanityData } from "@/sanity/lib/client";
+
+export interface Project {
+  _id: string;
+  title: string;
+  slug: string;
+  thumbnail: string;
+  shortDescription?: string;
+  tech?: string[];
+  type?: string;
+  demoLink?: string;
+  githubLink?: string;
+  featured?: boolean;
+}
+
+export async function fetchProjects(): Promise<Project[]> {
+  const query = `
+    *[_type == "project"] | order(_createdAt desc){
+      _id,
+      title,
+      "slug": slug.current,
+      "thumbnail": thumbnail.asset->url,
+      shortDescription,
+      tech,
+      type,
+      demoLink,
+      githubLink,
+      featured
+    }
+  `;
+  const data = await fetchSanityData(query);
+  return data ?? [];
+}
